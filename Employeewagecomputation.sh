084d4fdd3a122ec753e117a14b0190609c1588e1
unknown <sam1919>
2021-05-02 23:58:16 +0530

    Add:calculate daily employ wages

--- Employeewagecomputation.sh
+++ Employeewagecomputation.sh
@@ -12,4 +12,30 @@ function attendance()
 		echo "Employee is absent"
 	fi
 }
+function main()
+{
+
 attendance
+
+}
+
+main
+
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+randomCheck=$((RANDOM%3))
+
+
+if [ $isFullTime -eq $randomCheck ]
+then
+       empHrs=8
+elif [ $isPartTime -eq $randomCheck ]
+then
+      empHrs=8
+
+else
+      empHrs=0
+fi
+
+salary=$(($empHrs * $empRatePerHr))
